c175bbaa133f09fcfea341e729bd59dbf5e65d21
M suman kumar <suman10112004@gmail.com>
2026-07-15 23:36:26 +0530

    Time: 3 ms (93.03%), Space: 68.5 MB (87.46%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    
+    let length = nums.length;
+    const answer = new Array(length);
+        
+    let leftProduct = 1;
+    for (let i = 0; i < length; i++) 
+    {
+        answer[i] = leftProduct;
+        leftProduct *= nums[i];
+    }
+
+    let rightProduct = 1;
+    for (let i = length - 1; i >= 0; i--) 
+    {
+        answer[i] *= rightProduct;
+        rightProduct *= nums[i];
+    }
+        
+    return answer;
+
+};
